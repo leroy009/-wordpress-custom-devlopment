179626397ac85c41530089c594f11ef63745ec95
leroy <16562492+leroy009@users.noreply.github.com>
2024-07-15 10:32:04 +0200

    Creating the index page, post page, and page page, using the wordpress famous loop

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ if you are unable to add and delete plugins and themes
 
 Run ls -la /var/www/html/wordpress/ command to check the ownership of the wp-content directory
 AND PERHAPS change it using sudo chown -R www-data:www-data /var/www/html/wordpress/.
+
+so do thi chmod -R 777 the folder which gives you an issue
--- src/themes/leroy-premium-theme/single.php
+++ src/themes/leroy-premium-theme/single.php
@@ -2,9 +2,9 @@
     function showPosts() {
         while(have_posts()) {
            the_post(); ?>
-           <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+           <h1>This is a post</h1>
+           <h2><?php the_title(); ?></h2>
            <?php the_content(); ?>
-           <hr>
            <?php
         }
     }

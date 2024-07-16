876f13d075d7d5911e4abd96c633cc4361958229
leroy <16562492+leroy009@users.noreply.github.com>
2024-07-16 09:44:40 +0200

    Single post template

--- src/themes/leroy-premium-theme/single.php
+++ src/themes/leroy-premium-theme/single.php
@@ -4,11 +4,30 @@
 
     function showPosts() {
         while(have_posts()) {
-           the_post(); ?>
-           <h1>This is a post</h1>
-           <h2><?php the_title(); ?></h2>
-           <?php the_content(); ?>
-           <?php
+            the_post(); ?>
+            <div class="page-banner">
+                <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?> )"></div>
+                <div class="page-banner__content container container--narrow">
+                    <h1 class="page-banner__title"><?php the_title() ?></h1>
+                    <div class="page-banner__intro">
+                    <p>DO NOT FORGET TO REPLACE THE SUBTITLE LATER</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container container--narrow page-section">
+                <div class="metabox metabox--position-up metabox--with-home-link">
+                        <p>
+                            <a class="metabox__blog-home-link" href="<?php echo site_url('/blog') ?>"><i class="fa fa-home" aria-hidden="true">
+                            </i> Blog Home  </a> <span class="metabox__main">Posted by: <?php the_author_posts_link() ?> on the <?php the_time('j-M-Y'); ?> in <?php echo get_the_category_list(', ') ?></span>
+                        </p>
+                    </div>
+                <div class="genetic-content">
+                    <?php the_content() ?>
+                </div>
+            </div>
+
+            <?php
         }
     }
 

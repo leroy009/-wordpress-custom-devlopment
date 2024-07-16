784fb69fb9f8d92f128ce22547e01da58354a105
leroy <16562492+leroy009@users.noreply.github.com>
2024-07-16 14:10:02 +0200

    Custom post types updates

--- src/mu-plugins/leroy-post-types.php
+++ src/mu-plugins/leroy-post-types.php
@@ -2,8 +2,11 @@
 // Register custom post types
     function leroy_post_types() {
         register_post_type('event', array(
+            'supports' => array('title', 'editor', 'excerpt'), // add excerpts and things which this post should support
+            'rewrite' => array('slug' => 'events'), //Giving the post its own custom slug name
+            'has_archive' => true, // make this have archives or be in the archive page
             'public' => true,
-            'show_in_rest' => true, // Using The Modern Block Editor For Our Custom Post Type
+            'show_in_rest' => true,
             'labels' => array(
                 'name' => 'Events',
                 'add_new_item' => 'Add New Event',
@@ -16,4 +19,6 @@
     }
     add_action('init', 'leroy_post_types');
     // Register custom post types
+
+
 ?>

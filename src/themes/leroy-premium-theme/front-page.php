c3fdaab8017cf685565d99ef029715e2ef200384
leroy <16562492+leroy009@users.noreply.github.com>
2024-07-17 14:22:26 +0200

    Custom Fields

--- src/themes/leroy-premium-theme/front-page.php
+++ src/themes/leroy-premium-theme/front-page.php
@@ -33,12 +33,40 @@
                 ));
 
                 while($homepage_events->have_posts()) {
-                    $homepage_events->the_post(); ?>
+                    $homepage_events->the_post(); 
+
+                    // ACF 
+                    /*
+                    $event_date = new DateTime(get_gield('event_date));
+                    $event_month = $event_date->formart('M');
+                    $event_day = $event_date->formart('d');
+                    */
+
+                    // Jet Engine.
+                    $event_date = get_post_meta( get_the_ID(), 'event_date', true );
+
+                    // Check if the event date is valid
+                    if ( !empty( $event_date ) ) {
+                        try {
+                            $date = new DateTime('@' .$event_date);
+                            $date->setTimezone(new DateTimeZone('UTC'));
+                            $event_date_month = $date->format('M'); 
+                            $event_date_day = $date->format('d'); 
+                        } catch (Exception $e) {
+                            $event_date_month = '00';
+                            $event_date_day = '00';
+                        }
+                    } else {
+                        $event_date_month = '--';
+                            $event_date_day = '--';
+                    }
+                    // Jet Engine.
+                    ?>
 
                     <div class="event-summary">
                         <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                            <span class="event-summary__month">Mar</span>
-                            <span class="event-summary__day">25</span>
+                            <span class="event-summary__month"><?php echo esc_html( $event_date_month ); ?></span>
+                            <span class="event-summary__day"><?php echo esc_html( $event_date_day ); ?></span>
                         </a>
                         <div class="event-summary__content">
                             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>

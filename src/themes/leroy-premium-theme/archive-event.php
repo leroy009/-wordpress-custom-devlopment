314e052d2367938e4ccce6c032e6a0d939c80960
leroy <16562492+leroy009@users.noreply.github.com>
2024-07-23 10:06:37 +0200

    dynamic values for our custom events posts.

--- src/themes/leroy-premium-theme/archive-event.php
+++ src/themes/leroy-premium-theme/archive-event.php
@@ -15,11 +15,35 @@
     <div class="container container--narrow page-section">
         <?php
             while(have_posts()) {
-                the_post(); ?>
-                <div class="event-summary">
-                        <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                            <span class="event-summary__month">Mar</span>
-                            <span class="event-summary__day">25</span>
+                the_post(); 
+                
+                $event_date = get_post_meta( get_the_ID(), 'event_date', true );
+
+                if ( !empty( $event_date ) ) {
+                    try {
+                        // $date = new DateTime('@' .$event_date); // Time Stamp
+                        $date = new DateTime($event_date);
+                        $date->setTimezone(new DateTimeZone('Africa/Johannesburg'));
+                        $event_date_month = $date->format('M'); 
+                        $event_date_day = $date->format('d'); 
+                        $event_date_year = $date->format('Y'); 
+                    } catch (Exception $e) {
+                        $event_date_month = '00';
+                        $event_date_day = '00';
+                        $event_date_year = '00';
+                    }
+                } else {
+                    $event_date_month = '--';
+                    $event_date_day = '--';
+                    $event_date_year = '--';
+                }
+                
+                ?>
+                <div class="event-summary" style="margin-bottom: 30px;">
+                        <a class="event-summary__date t-center" style="width: 100px;" href="<?php the_permalink(); ?>">
+                            <span class="event-summary__month"><?php echo esc_html( $event_date_month ); ?></span>
+                            <span class="event-summary__day"><?php echo esc_html( $event_date_day ); ?></span>
+                            <span class="event-summary__month"><?php echo esc_html( $event_date_year ); ?></span>
                         </a>
                         <div class="event-summary__content">
                             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>

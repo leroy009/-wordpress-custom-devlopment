87e9a4f034730c86c2cfbc8a53ce82f95c2c5e83
leroy <16562492+leroy009@users.noreply.github.com>
2024-07-23 16:49:56 +0200

    Custom Relationships and Displaying Relationships

--- src/themes/leroy-premium-theme/single-event.php
+++ src/themes/leroy-premium-theme/single-event.php
@@ -25,6 +25,23 @@
                 <div class="genetic-content">
                     <?php the_content() ?>
                 </div>
+                <?php 
+                    // $relatedPrograms =  get_field('related_program'); // ACF
+                    $relatedPrograms =  get_post_meta( get_the_ID(), 'related_programs', true ); 
+
+                    if ($relatedPrograms) { ?>
+
+                    <hr class="section-break">
+                    <h2 class="headline headline--medium" >Related Program(s)</h2>
+                    <ul class="link-list min-list">
+                    <?php
+                        foreach($relatedPrograms as $program) { ?>
+                            <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+                            
+                        <?php } 
+                    } ?>
+                    </ul>
+                
             </div>
 
             <?php
--- src/themes/leroy-premium-theme/single-program.php
+++ src/themes/leroy-premium-theme/single-program.php
@@ -25,6 +25,101 @@
                 <div class="genetic-content">
                     <?php the_content() ?>
                 </div>
+
+                <?php 
+            
+                $today = date('Y-m-d\TH:i');
+                $homepage_events = new WP_Query(array(
+                    'posts_per_page' => 2,
+                    'post_type' => 'event',
+                    'meta_key' => 'event_date',
+                    'orderby' => 'meta_value',
+                    'meta_query' => array(
+                        array(
+                            'key' => 'event_date',
+                            'compare' => '>=',
+                            'value' => $today,
+                            'type' => 'DATETIME'
+                        ), array(
+                            'key' => 'related_program',
+                            'compare' => 'LIKE',
+                            'value' => '"' . get_the_ID() . '"'
+
+                        )
+                    ),
+                    // custom meta fileds sorting
+                    'order' => 'ASC'
+                ));
+                
+                if ($homepage_events->have_posts()) {
+
+                echo '<hr>';
+                echo '<h2 class="headline headline--medium" style="margin-top: 20px;">Upcoming ' . get_the_title() . '</h2>';
+
+                while($homepage_events->have_posts()) {
+                    $homepage_events->the_post(); 
+                    // $post_id = get_the_ID();
+
+                    // ACF 
+                    /*
+                    $event_date = new DateTime(get_gield('event_date));
+                    $event_month = $event_date->formart('M');
+                    $event_day = $event_date->formart('d');
+                    */
+
+                    // Jet Engine.
+                    $event_date = get_post_meta( get_the_ID(), 'event_date', true );
+                    // echo 'Event date: ';
+                    // var_dump($event_date); // Debug output
+
+                    // Check if the event date is valid
+                    if ( !empty( $event_date ) ) {
+                        try {
+                            // $date = new DateTime('@' .$event_date); // Time Stamp
+                            $date = new DateTime($event_date);
+                            $date->setTimezone(new DateTimeZone('Africa/Johannesburg'));
+                            $event_date_month = $date->format('M'); 
+                            $event_date_day = $date->format('d'); 
+                            $event_date_year = $date->format('Y'); 
+                        } catch (Exception $e) {
+                            $event_date_month = '00';
+                            $event_date_day = '00';
+                            $event_date_year = '00';
+                        }
+                    } else {
+                        $event_date_month = '--';
+                        $event_date_day = '--';
+                        $event_date_year = '--';
+                    }
+                    // Jet Engine.
+
+
+                    ?>
+                    
+                    <div class="event-summary" style="margin-bottom: 30px; ">
+                        <a class="event-summary__date t-center" style="width: 100px;" href="<?php the_permalink(); ?>">
+                            <span class="event-summary__month"><?php echo esc_html( $event_date_month ); ?></span>
+                            <span class="event-summary__day"><?php echo esc_html( $event_date_day ); ?></span>
+                            <span class="event-summary__month"><?php echo esc_html( $event_date_year ); ?></span>
+                        </a>
+                        <div class="event-summary__content">
+                            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
+                            <p><?php if (has_excerpt()) {
+                            echo get_the_excerpt();
+                        } else {
+                            echo wp_trim_words(get_the_content(), 18);
+                        } 
+                        // echo wp_trim_words(get_the_excerpt(), 20);
+                        ?>  <a href="<a href="<?php the_permalink() ?>" class="nu gray">Learn more</a></p>
+                            <!-- <p><?php echo wp_trim_words(get_the_excerpt(), 15) ?> <a href="<a href="<?php the_permalink() ?>" class="nu gray">Learn more</a></p> -->
+                        </div>
+                    </div>
+
+                <?php wp_reset_postdata();
+                }
+            }
+            ?>
+
             </div>
 
             <?php
